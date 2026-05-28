56178bd20ec47b12b330031de9bde533bb7c98f0
Kipchumba Bett <kbett1@safaricom.co.ke>
2026-05-28 16:40:25 +0300

    Fix security configurations

--- src/main/java/ke/co/safaricom/pims/inventory/config/SecurityConfig.java
+++ src/main/java/ke/co/safaricom/pims/inventory/config/SecurityConfig.java
@@ -1,20 +1,18 @@
 package ke.co.safaricom.pims.inventory.config;
 
 import ke.co.safaricom.pims.inventory.Constants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Profile("!dev")
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class SecurityConfig {
 
     @Bean
